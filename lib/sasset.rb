5c8bc1f526266621c44496b2528be163d97f2780
Davis W. Frank & Tom Bogart <pair+dwfrank+tom@pivotallabs.com>
2011-11-01 11:00:20 -0700

    Upgrade asset to handle the new asset pipeline

--- lib/sasset.rb
+++ lib/sasset.rb
@@ -1,19 +1,19 @@
 require 'sass'
+require 'version'
 
 module Sasset
-  VERSION = '1.0.0'
-
   class << self
     attr_accessor :asset_host
 
     def asset_url(source)
+      source = "assets/#{source}"
       if asset_host.respond_to? :call
         prefix = asset_host.call(source)
       else
         prefix = asset_host || ''
       end
 
-      mtime = File.new(File.join(Rails.public_path, source)).mtime.to_i
+      mtime = File.new(File.join(Rails.root, 'app', source)).mtime.to_i
       File.join(prefix, "#{source}?#{mtime}")
     end
   end
